08057e2375dda6cbffc28ece55104663b6130d55
Rishub Jha <rishubjha34@gmail.com>
2024-11-10 18:06:45 +0530

    Adding project to git with modular code for features, models and service

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An ml project to predict the trip duration for nyc taxi servise.',
+    author='Rishub Jha',
+    license='MIT',
+)
